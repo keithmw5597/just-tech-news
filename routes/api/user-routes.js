69d9610041b40c715bab8f9dd825966ca73b546f
keithmw <keithmw5597@yahoo.com>
2022-06-12 10:14:52 -0500

    login

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -30,7 +30,10 @@ router.get('/:id', (req, res) => {
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
+
+
     });
+    
 });
 
 router.post('/', (req, res) => {
@@ -46,6 +49,32 @@ router.post('/', (req, res) => {
       res.status(500).json(err);
     });
 });
+
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+      const validPassword = dbUserData.checkPassword(req.body.password);
+
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+      // res.json({ user: dbUserData });
+  
+      // Verify user
+
+    });  
+  });
 
 router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
